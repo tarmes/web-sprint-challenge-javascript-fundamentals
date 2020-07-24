06111c661a05beda105172c7732c9245e27a7727
tarmes <traarmes@gmail.com>
2020-07-24 12:05:39 -0400

    Closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,21 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+// nestedFunction() and internal are both scoped the same. Within the closure of myFunction()
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(n){
+  let f = 0;
+  for (let i = 1; i <= n; i++){
+    f = f + i;
+  }
+  return f;
+}
+
+console.log(summation(10));
+
+// Wasn't sure if they wanted us to use reduce() or something, but they didn't specify so I just used a for loop.
